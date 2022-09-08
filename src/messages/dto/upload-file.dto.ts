7c305ce0a56ea0bc866b6120b784346703952202
Vitaly <vitalyryabchich@gmail.com>
2022-09-08 21:14:16 +0300

    upload files

--- src/messages/dto/upload-file.dto.ts
+++ src/messages/dto/upload-file.dto.ts
@@ -1,6 +1,7 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { CreateMessageDto } from './create-message.dto';
 
-export class UploadFileDto {
+export class UploadFileDto extends PickType(CreateMessageDto, ['from', 'to']) {
   @ApiProperty({ format: 'binary', type: 'string' })
   file: Express.Multer.File;
 }
